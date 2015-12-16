c85abb18cbaddd7a3fcce77ca2cef06950160ac4
sdf00002 <sdf00002@red.ujaen.es>
2015-12-16 13:22:47 +0100

    Eliminación de clases innecesarias
    Se han eliminado las clases que no se utilizaban. La clase Cliente sería
    un cliente de prueba para Java.

---
+++ Servidor/src/server/Cliente.java
@@ -0,0 +1,71 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class Cliente {
+	
+	public static void main(String argv[]) throws Exception {
+		String user, pass, cmd = "", num;
+		String modifiedSentence = "";
+		int estado=0;
+		byte v=0;
+		final byte MSG_LOGIN=0x01; 
+		final byte MSG_OPERACION=0x02;
+		final byte MSG_FIN=0X04;
+		final byte version=1;
+		
+		try {
+			Socket clientSocket = new Socket("localhost", 7000);
+			
+			byte tipo = 1;
+			int secuencia = 1;
+			DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+			BufferedReader inFromUser = new BufferedReader(
+                    new InputStreamReader(System.in));
+			DataInputStream dis = new DataInputStream(clientSocket.getInputStream());
+		
+			
+			while (estado==0) {
+				
+				System.out.println("Introduce usuario");
+				user = inFromUser.readLine();
+				System.out.println("Introduce contraseña");
+				pass = inFromUser.readLine();
+				System.out.println("Introduce un valor");
+				num = inFromUser.readLine();
+
+				
+
+				outToServer.write(version);
+
+				outToServer.writeInt(secuencia);
+
+				outToServer.write(MSG_OPERACION);
+				outToServer.writeUTF("sin");
+				outToServer.writeUTF(user + "_" + pass+"_"+num);
+				outToServer.flush();
+				
+				modifiedSentence=dis.readUTF();																			
+				
+				//System.out.println("Version: "+v+" secuencia: "+secuencia+" tipo: "+tipo+" cmd: "+cmd+" res: "+res);
+				System.out.println(modifiedSentence);
+				System.out.println(modifiedSentence.length());
+				estado++;
+			}
+
+
+			System.out.println("Has salido");
+			dis.close();
+			outToServer.close();
+			clientSocket.close();
+		} catch (IOException e) {
+
+		}
+	}
+
+}
--- Servidor/src/server/Connection.java
+++ Servidor/src/server/Connection.java
@@ -1,30 +1,27 @@
 package server;
 
-import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.SocketException;
 
-
 public class Connection implements Runnable {
 
-	
 	//PDU formada por: Version+secuencia+tipo+comando+payload
 	
-		//Comandos utilizados en el protocolo
-		public static final String CRLF="\r\n";
-		public static final String OK="+OK";
-		public static final String ERR="-ERR";
-		public static final String QUIT="QUIT";
-		//Tipos de mensajes
-		public static final byte MSG_LOGIN=0x01; 
-		public static final byte MSG_OPERACION=0x02;
-		public static final byte MSG_FIN=0X04;
-	
+	//Comandos utilizados en el protocolo
+	public static final String CRLF="\r\n";
+	public static final String OK="+OK";
+	public static final String ERR="-ERR";
+	public static final String QUIT="QUIT";
+	//Tipos de mensajes
+	public static final byte MSG_LOGIN=0x01; 
+	public static final byte MSG_OPERACION=0x02;
+	public static final byte MSG_FIN=0X04;
+
 	Socket mSocket;
-	public static String MSG_WELCOME = OK+" Bienvenido al servidor de pruebas"+CRLF;
+
 
 
 	public Connection(Socket s) {
@@ -35,151 +32,123 @@ public class Connection implements Runnable {
 	public void run() {
 		String inputData = null;
 		String outputData = "";
-		String comando,payload,user="",pass="";
+		String comando,payload,user="",pass="",valor="";
 		byte version=0, tipo=0,VERSION=1;
 		int estado = 0,secuencia;
 		Authentication auth = new Authentication("");
 		if (mSocket != null) {
 			try {
 
-				DataOutputStream output = new DataOutputStream(mSocket.getOutputStream());
-				BufferedReader input = new BufferedReader(new InputStreamReader(mSocket.getInputStream()));
+			DataOutputStream output = new DataOutputStream(mSocket.getOutputStream());
+			//BufferedReader input = new BufferedReader(new InputStreamReader(mSocket.getInputStream()));
+			DataInputStream input = new DataInputStream(mSocket.getInputStream());
+			
 
-				output.write(MSG_WELCOME.getBytes());
+			while (estado==0) {
+				
+				try{
+					version = input.readByte();
+					if(version!=1){
+						//outputData = ERR+" [" + version + "] VERSION NO VALIDA";
+						String aux="Version no valida";
+						output.write(VERSION);
+						output.writeInt(1);
+						output.write(MSG_FIN);
+						output.writeUTF(ERR);
+						output.writeUTF(aux);
+						break;
 
-				while ((inputData = input.readLine()) != null) {
-					System.out.println("Servidor [Recibido]> " + inputData);
-					String campos[] = inputData.split(" ");
-					
-					if (campos.length==1){
-						if(inputData.equalsIgnoreCase("")||inputData.equalsIgnoreCase(QUIT)){
-							outputData="Has salido";
-							break;
-						}
-						else outputData = "ERROR [" + inputData + "] NO ES UN COMANDO VALIDO"+CRLF;
 					}
-					
-					else if (campos.length == 5) {
-						try{
-						version = Byte.parseByte(campos[0]);
-						} catch(NumberFormatException e){
-							//Acciones asociadas a una version invalida
-							outputData = ERR+" [" + version + "] FORMATO DE VERSION NO VALIDO"+CRLF;
-							break;
-						}
-						secuencia = Integer.parseInt(campos[1]);
-						tipo = Byte.parseByte(campos[2]);
-						comando = campos[3];
-						payload = campos [4];
+					} catch(NumberFormatException e){
+						//Acciones asociadas a una version invalida
+						//outputData = ERR+" [" + version + "] FORMATO DE VERSION NO VALIDO"+CRLF;
+						String aux="Formato version no valido";
+						output.write(VERSION);
+						output.writeInt(1);
+						output.write(MSG_FIN);
+						output.writeUTF(ERR);
+						output.writeUTF(aux);
+						output.flush();
+						break;
+					}
+				
+					secuencia=input.readInt();
+					tipo=input.readByte();
+					comando=input.readUTF();
+					payload=input.readUTF();
 
-						switch (estado) {
 
-						case 0:
-							if (version==1 && tipo== MSG_LOGIN && comando.equalsIgnoreCase(OK)) {
-								//Comprobamos el usuario y su contraseña
-								String[] credencial = payload.split("_");
-								user=credencial[0];
-								pass = credencial[1];
-								auth = new Authentication(user);
+
+							//Comprobamos el usuario y su contraseña
+							String[] credencial = payload.split("_");
+							user=credencial[0];
+							pass = credencial[1];
+							valor=credencial[2];
+							auth = new Authentication(user);
+							
+							if(tipo==MSG_OPERACION){
+							//Comprobamos que exista el usuario y que la contraseña sea correcta
+							if (auth.open(user)==true && auth.checkKey(pass) == true) {	
+								//outputData =String.valueOf(VERSION) + secuencia + MSG_LOGIN + OK + "Realiza la operacion";
+								if(comando.equalsIgnoreCase("sin")){
+									double res=Math.sin(Double.parseDouble(valor)*Math.PI/180);
+									outputData =String.valueOf(VERSION) + secuencia + MSG_FIN + OK + comando + res;
+									
+								}
+								else if (comando.equalsIgnoreCase("cos")){
+									double res=Math.cos(Double.parseDouble(valor)*Math.PI/180);
+									outputData =String.valueOf(VERSION) + secuencia + MSG_FIN + OK + comando + res;
+									
+								}
+								
+									else{
+										outputData =String.valueOf(VERSION) + secuencia + MSG_FIN + ERR + "comando incorrecto";
+								
+									
+									}
+								
+							}
+							else if (auth.open(user)==false)
+								outputData =String.valueOf(VERSION) + secuencia + MSG_FIN + ERR + "usuario no existente";
+								else if (auth.checkKey(pass) == false)
+									outputData =String.valueOf(VERSION) + secuencia + MSG_FIN + ERR + "password incorrecta";					
+							
+							}
+							
+							else if(tipo==MSG_LOGIN){
 								//Comprobamos que exista el usuario y que la contraseña sea correcta
 								if (auth.open(user)==true && auth.checkKey(pass) == true) {	
-									outputData = CRLF+VERSION + secuencia + 1 + MSG_LOGIN + OK + "Realiza la operacion"+CRLF;
-									
-									estado++;
+									outputData =String.valueOf(VERSION) + secuencia + MSG_FIN + OK + "autenticado";
 								}
-								
-								else if (auth.open(user)==false){
-									outputData = ERR+"[" + user + "] NO ES UN USUARIO REGISTRADO"+CRLF;
-									
-								}
-								
-								else if (auth.open(user)==true && auth.checkKey(pass) == false){
-									outputData = ERR+"[" + user + "] CONTRASENA INCORRECTA+CRLF";
-								}
-								
-								else if (tipo!=MSG_LOGIN){
-									outputData = ERR+" [" + inputData + "] TIPO DE MENSAJE INCORRECTO"+CRLF;
-									
-								}
-								
-							//Hacer comprobacion version tipo y comando
+								else if (auth.open(user)==false)
+									outputData =String.valueOf(VERSION) + secuencia + MSG_FIN + ERR + "usuario no existente";
+									else if (auth.checkKey(pass) == false)
+										outputData =String.valueOf(VERSION) + secuencia + MSG_FIN + ERR + "password incorrecta";
 							}
 							
-							else if (tipo!=MSG_LOGIN){
-								outputData = ERR+" [" + inputData + "] TIPO DE MENSAJE INCORRECTO"+CRLF;
-								
-							}
-							
-							else if(comando!=OK){
-								outputData = ERR+" [" + comando + "] COMANDO INCORRECTO"+CRLF;
+							output.writeUTF(outputData);
+							output.flush();
+							estado++;
 
-							}
-							
-							break;
-						
-
-						case 1:
-
-							if (comando.length() == 3 && version==1 && tipo==MSG_OPERACION) {
-								if (comando.equalsIgnoreCase("SIN")) {
-
-									try {
-										String sin = String
-												.valueOf(Math.sin(Double.parseDouble(payload)));
-										outputData = "OK " + comando + " el seno  es = " + sin +CRLF;
-
-									} catch (NumberFormatException ex) {
-										outputData = ERR+" FORMATO DE NUMERO INCORRECTO\r\n";
-									}
-								} else if(comando.equalsIgnoreCase("COS")){
-									try {
-										String cos = String
-												.valueOf(Math.cos(Double.parseDouble(payload)));
-										outputData = CRLF+"OK " + comando + " el coseno  es = " + cos + CRLF;
-
-									} catch (NumberFormatException ex) {
-										outputData = ERR+" FORMATO DE NUMERO INCORRECTO"+CRLF;
-									}
-								}
-								
-								else
-									outputData = ERR+" COMANDO DESCONOCIDO"+CRLF;
-									
-							}
-							else if(version!=1)
-								outputData = ERR+" VERSION INCORRECTA"+CRLF;
-							
-							else if (tipo!=MSG_OPERACION)
-								outputData = ERR+" TIPO DE MENSAJE INCORRECTO"+CRLF;
-							
-							else
-								outputData = ERR+" COMANDO DESCONOCIDO"+CRLF;
-							break;
-						}
-						
-					} else
-						outputData = ERR+" [" + inputData + "] NO ES UN COMANDO VALIDO"+CRLF;
-
-					output.write(outputData.getBytes());
-
-				}
-				output.write(outputData.getBytes());
-				System.out.println(
-						"Servidor [Finalizado]> " + mSocket.getInetAddress().toString() + ":" + mSocket.getPort());
-
-				input.close();
-				output.close();
-				mSocket.close();
-			} catch (SocketException se) {
-
-				se.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
+			//output.write(outputData.getBytes());
+			System.out.println(
+					"Servidor [Finalizado]> " + mSocket.getInetAddress().toString() + ":" + mSocket.getPort());
 
+			input.close();
+			output.close();
+			mSocket.close();
+		} catch (SocketException se) {
+
+			se.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
 	}
 
+}
+	
+	
 }
